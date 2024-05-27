1425596524f9f7585b545a28528c0ae4de4d51ee
Rambeybello <ramadan@myitura.com>
2024-05-27 15:52:33 +0100

    ..

--- src/baseDir/index.ts
+++ src/baseDir/index.ts
@@ -1,5 +1,6 @@
 export const BaseDir = {
-  API_BASE_URL: import.meta.env.VITE_APP_API_BASEURL,
+  API_BASE_URL: "http://info-payroll.infonomicsng.com:8081/api/v1",
+  // API_BASE_URL: import.meta.env.VITE_APP_API_BASEURL,
 
   IMAGE_DIR: "/assets/image",
   SVG_DIR: "/assets/svg",
